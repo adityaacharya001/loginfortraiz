56a7e6da0c84322621b2c97613626bc438ffd8b9
adityaacharya001 <30295883+adityaacharya001@users.noreply.github.com>
2017-10-08 01:10:31 +0530

    First commit

---
+++ static/src/components/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Home = () =>
+    <section>
+        <div className="container text-center">
+            <h1>Welcome to Fortraiz</h1>
+        </div>
+    </section>;
